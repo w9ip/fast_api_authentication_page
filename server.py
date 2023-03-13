b79d5f8647cdb07be0aade788ae396882742c0da
w9ip <m.p1nk@yandex.ru>
2023-03-13 16:14:47 +0300

    login page

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Form
 from fastapi.responses import Response
 
 
@@ -17,3 +17,13 @@ def index_page():
         media_type="text/html",
     )
 
+
+@app.post("/login")
+def process_login_page(
+    username : str = Form(...), 
+    password : str = Form(...)
+    ):
+    return Response(
+        f"Твой логин: {username}, Твой пароль: {password}",
+        media_type="text/html",     
+    )
