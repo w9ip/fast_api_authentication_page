f86adf93310bd40509ff10b042b5971d4edca7bb
w9ip <114592058+w9ip@users.noreply.github.com>
2023-03-14 13:46:45 +0300

    Update server.py

--- server.py
+++ server.py
@@ -26,11 +26,11 @@ users = {
 SECRET_KEY = "c727ed77f2fd8ddbc99646dfd62420c241319506b56acad4e9c7cd5b0a35e644"
 PASSWORD_SALT = "2936c7701d34684b1f6caefc7bcffd5a76e8d8ff31d99c6b7ddd3cb3e5e6a79c"
 
+
 def verify_password(username: str, password: str) -> bool:
     password_hash = hashlib.sha256( (password + PASSWORD_SALT).encode() ).hexdigest().lower()
     stored_password_hash = users[username]["password"].lower()
     return password_hash == stored_password_hash
-
 
 
 def signed_data(data: str) -> str:
@@ -44,46 +44,42 @@ def signed_data(data: str) -> str:
 
 def get_username_from_signed_string(username_signed: str) -> Optional[str]: 
     """Возвращает обычный username от шифрованной"""
-    username_base64, sign = username_signed.split(".")  # Разобьем строку
-    username = base64.b64decode(username_base64.encode()).decode()  # Декодируем base64
-    valid_sign = signed_data(username)  # Получаем валидную подпись 
-    if hmac.compare_digest(valid_sign, sign):  # Сравниваем валидную подпись с тем что пришло (ожидаем одинаковые данные)
-        return username  # Если пришли валидные данные - вернуть обычную строку
-    # Иначе, ничего не возвращаем
+    username_base64, sign = username_signed.split(".") 
+    username = base64.b64decode(username_base64.encode()).decode() 
+    valid_sign = signed_data(username)  
+    if hmac.compare_digest(valid_sign, sign):
+        return username 
+    
 
 @app.get("/")
 def index_page(
-    username: Optional[str] = Cookie(default=None)  # Если куки не установлены то Cookie == None
+    username: Optional[str] = Cookie(default=None)
     ):
     """A main site's page"""
     
     with open('templates/login.html', 'r') as f:
         html_login_page = f.read()
     
-    if not username:  # Если в куку ничего не прилетело!
+    if not username:
         return Response(
-        html_login_page, # То вернем пользователя на начальную страницу
+        html_login_page, 
         media_type="text/html",
     )
-    # Иначе если (проверка на валидный юзернейм)...
     valid_username = get_username_from_signed_string(username)
     if not valid_username:
-        response = Response(html_login_page, media_type='text/html')  # Если нет
-        response.delete_cookie(key="username")                        # Удаляем куки
-        return response                                               # Возвращаем на начальную страницу
-    # Иначе если юзернейм валидный
-    try:  # В случае если нет или есть такой пользователь в нашем словаре
+        response = Response(html_login_page, media_type='text/html')  
+        response.delete_cookie(key="username")                        
+        return response                                               
+    try: 
         user = users[valid_username]
     except KeyError:
-        response = Response(html_login_page, media_type='text/html')  # Если нет
-        response.delete_cookie(key="username")                        # Удаляем куки
-        return response                                               # Возвращаем на начальную страницу
-    # Но если есть такой пользователь
+        response = Response(html_login_page, media_type='text/html')
+        response.delete_cookie(key="username")
+        return response
     return Response(
-        f"Привет, {users[valid_username]['name']}!",  # То приветствуем пользователя!
+        f"Привет, {users[valid_username]['name']}!",
         media_type='text/html',
         )
-
 
 
 @app.post("/login")
@@ -91,13 +87,13 @@ def process_login_page(
     username : str = Form(...), 
     password : str = Form(...),
     ):
-    user = users.get(username)  # Пробуем получить такого пользователя из БД, если такого ключа нет, .get() вернет None
-    if not user or not verify_password(username, password):  # Если user == None или если не verify_password(username, password)
-        return Response(  # То вернуть сообщение -> "Я вас не знаю!"
+    user = users.get(username)
+    if not user or not verify_password(username, password):
+        return Response(
             'Я вас не знаю!',
             media_type="text/html",
         )
-    response = Response(  # Иначе, авторизовать пользователя и поприветствовать нашего красавчика!
+    response = Response(
         f"Привет, {user['name']}!<br /> Баланс: {user['balance']}",
         media_type="text/html",     
     )
